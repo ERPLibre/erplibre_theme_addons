d4edb9d99a63fab40df03537fb9bb15321c30e3f
Mathieu Benoit <mathben@technolibre.ca>
2022-03-03 23:38:48 -0500

    [ADD] second commit

---
+++ website_cpcouleurs_data/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "Demo website data",
+    "category": "Website",
+    "summary": "Exported Data from website",
+    "version": "12.0.1.0",
+    "author": "TechnoLibre",
+    "license": "AGPL-3",
+    "website": "https://technolibre.ca",
+    "depends": ["website"],
+    "data": [
+        "data/ir_ui_view.xml",
+        "data/website_page.xml",
+        "data/website_menu.xml",
+    ],
+    "installable": True,
+    "pre_init_hook": "pre_init_hook",
+}
--- website_cpcouleurs_theme/__manifest__.py
+++ website_cpcouleurs_theme/__manifest__.py
@@ -3,6 +3,7 @@
     "version": "12.0.1.0",
     "author": "TechnoLibre",
     "license": "AGPL-3",
+    "depends": ["web_editor", "website_cpcouleurs_data"],
     "installable": True,
     "post_init_hook": "post_init_hook",
 }
