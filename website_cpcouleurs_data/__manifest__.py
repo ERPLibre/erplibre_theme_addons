2ba47b959c3330805cbd4164b46db7149512f525
Mathieu Benoit <mathben@technolibre.ca>
2022-03-04 23:58:44 -0500

    [ADD] website_cpcouleurs_data

---
+++ website_cpcouleurs_data/__manifest__.py
@@ -0,0 +1,26 @@
+{
+    "name": "Demo website data",
+    "category": "Website",
+    "summary": "Exported Data from website",
+    "version": "12.0.1.0",
+    "author": "TechnoLibre",
+    "license": "AGPL-3",
+    "website": "https://technolibre.ca",
+    "depends": [
+        "website",
+        "base",
+        "calendar",
+        "mail",
+        "muk_utils",
+        "muk_web_utils",
+        "web_editor",
+    ],
+    "data": [
+        "data/ir_attachment.xml",
+        "data/ir_ui_view.xml",
+        "data/website_page.xml",
+        "data/website_menu.xml",
+    ],
+    "installable": True,
+    "pre_init_hook": "pre_init_hook",
+}
