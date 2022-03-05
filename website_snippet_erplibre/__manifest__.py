e2c94aa1520a15e83e515b852c10d1f0c2dab732
Mathieu Benoit <mathben@technolibre.ca>
2022-03-04 23:57:23 -0500

    [ADD] website_snippet_erplibre NOT WORKING

---
+++ website_snippet_erplibre/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "Website Snippet Erplibre",
+    "category": "Website",
+    "version": "12.0.1.0",
+    "author": "TechnoLibre",
+    "license": "AGPL-3",
+    "website": "https://technolibre.ca",
+    "application": True,
+    "depends": ["website", "theme_common"],
+    "data": ["views/snippets.xml", "views/assets.xml"],
+    "installable": True,
+}
